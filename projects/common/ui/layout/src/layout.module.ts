cd8612d935fc3621ba741c5706119ee636f18a69
iraklis <iraklis.bekiaris@kebormed.com>
2022-12-19 10:14:52 +0200

    Side-nav structure

--- projects/common/ui/layout/src/layout.module.ts
+++ projects/common/ui/layout/src/layout.module.ts
@@ -1,9 +1,22 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [],
+  declarations: [
+    LayoutComponent,
+    SideNavComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    CommonModule,
+    MatSidenavModule
+  ],
   exports: [LayoutComponent],
 })
-export class LayoutModule {}
+export class LayoutModule { }
